0ba1af2b4b006f2063fbce2950aa4e42485c7e45
junghwankim <padd60@naver.com>
2021-10-18 12:06:18 +0900

    java : Haksa manage program save

--- govermentEdu_Java/Haksa2/src/Haksa2.java
+++ govermentEdu_Java/Haksa2/src/Haksa2.java
@@ -1,14 +1,6 @@
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -18,8 +10,8 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
@@ -75,366 +67,20 @@ class LoginDialog extends JDialog{
 }
 
 public class Haksa2 extends JFrame{
-	JTextField txtId = null;
-	JTextField txtName = null;
-	JTextField txtDepartment = null;
-	JTextField txtAddress = null;
-	
+	 JPanel panel;  // 메뉴별 화면이 출력되는 패널	
 	LoginDialog login;
-	
-	// CRUD 
-	JButton btnInsert = null; // 등록 Create
-	JButton btnSelect = null; // 목록 Read
-	JButton btnUpdate = null; // 수정 Update
-	JButton btnDelete = null; // 삭제 Delete
-	
-	//search
-	JButton btnSearch = null;
-	
-	//table, model
-	DefaultTableModel model=null; // 테이블의 데이터
-	JTable table=null;
-	
-
-	
+		
 	public Haksa2() {
 		this.setTitle("학사관리프로그램");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		this.setLayout(new FlowLayout());
-		
+				
 		this.createMenu();
 		login = new LoginDialog(this,"로그인");
 		
-		this.add(new JLabel("학번"));
-		this.txtId = new JTextField(15);
-		this.add(txtId);
+		panel = new JPanel();
+		this.add(panel);
 		
-		this.btnSearch = new JButton("검색");
-		this.add(btnSearch);
-		this.btnSearch.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try { 
-					Class.forName("com.mysql.jdbc.Driver");
-					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampledb", "root","58545256");
-					System.out.println("연결 성공");
-					
-					Statement stmt = conn.createStatement();
-					
-					
-					ResultSet rs = stmt.executeQuery("select * from student where id = '"+ txtId.getText() +"' order by name");
-					
-				    //JTable 초기화
-				    model.setNumRows(0);
-					
-					while(rs.next()) {
-//						System.out.println(rs.getString("id")); 
-//						System.out.println(rs.getString("name")); 
-//						System.out.println(rs.getString("dept"));
-						
-//						taList.append(rs.getString("id") + '\t' + rs.getString("name") + '\t' + rs.getString("dept") + '\n');
-						
-						String [] row = new String[4];
-						row[0]=rs.getString("id");
-						row[1]=rs.getString("name");
-						row[2]=rs.getString("dept");
-						row[3]=rs.getString("address");
-						model.addRow(row);
-						
-						txtId.setText(rs.getString("id"));
-						txtName.setText(rs.getString("name"));
-						txtDepartment.setText(rs.getString("dept"));
-						txtAddress.setText(rs.getString("address"));
-						}
-					rs.close();
-					stmt.close();
-					conn.close();
-				} catch (ClassNotFoundException e1) { e1.printStackTrace();
-				} catch (SQLException e1) {
-					System.out.println("연결 실패");
-					e1.printStackTrace();
-				}
-				
-			}
-			
-		});
-		
-		this.add(new JLabel("이름"));
-		this.txtName = new JTextField(22);
-		this.add(txtName);
-		
-		this.add(new JLabel("학과"));
-		this.txtDepartment = new JTextField(22);
-		this.add(txtDepartment);
-		
-		this.add(new JLabel("주소"));
-		this.txtAddress = new JTextField(22);
-		this.add(txtAddress);
-		
-		String[] colname= {"학번", "이름", "학과", "주소"};
-		this.model=new DefaultTableModel(colname,0);
-		this.table=new JTable(model); // model과 table 바인딩
-		table.setPreferredScrollableViewportSize(new Dimension(300,250)); //table 크기
-		this.add(table);
-		JScrollPane sp=new JScrollPane(table); // 스크롤 구현
-	    this.add(sp);
-		
-	    this.table.addMouseListener(new MouseListener() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				table=(JTable)e.getComponent();
-				model=(DefaultTableModel)table.getModel();
-				txtId.setText((String)model.getValueAt(table.getSelectedRow(), 0)); // 학번
-				txtName.setText((String)model.getValueAt(table.getSelectedRow(), 1)); // 이름
-				txtDepartment.setText((String)model.getValueAt(table.getSelectedRow(), 2)); // 학과
-				txtAddress.setText((String)model.getValueAt(table.getSelectedRow(), 3)); // 주소
-			} 
-
-			@Override
-			public void mousePressed(MouseEvent e) {}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {}
-
-			@Override
-			public void mouseExited(MouseEvent e) {}
-	    	
-	    });
-		
-		this.btnInsert = new JButton("등록");
-		this.add(btnInsert);
-		this.btnInsert.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// sql 연동
-				try { 
-					Class.forName("com.mysql.jdbc.Driver");
-					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampledb", "root","58545256");
-					System.out.println("연결 성공");
-					
-					Statement stmt = conn.createStatement();
-					
-					//insert
-					stmt.executeUpdate("insert into student values('"+ txtId.getText() +"', '"+ txtName.getText() +"', '"+ txtDepartment.getText() + "', '" + txtAddress.getText() + "')");
-					
-					//update
-					//stmt.executeUpdate("update student set name='홍길동' where id = '1234567'");
-					
-					//delete
-					//stmt.executeUpdate("delete from student where id = '1234567'");
-					
-
-					
-					ResultSet rs = stmt.executeQuery("select * from student order by name");
-					
-				    //JTable 초기화
-				    model.setNumRows(0);
-					
-					while(rs.next()) {
-						
-//						taList.append(rs.getString("id") + '\t' + rs.getString("name") + '\t' + rs.getString("dept") + '\n');
-						
-						String [] row = new String[4];
-						row[0]=rs.getString("id");
-						row[1]=rs.getString("name");
-						row[2]=rs.getString("dept");
-						row[3]=rs.getString("address");
-						model.addRow(row);
-					}
-					rs.close();
-					stmt.close();
-					conn.close();
-				} catch (ClassNotFoundException e1) { e1.printStackTrace();
-				} catch (SQLException e1) {
-					System.out.println("연결 실패");
-					e1.printStackTrace();
-				}
-				
-				JTextField [] check = {txtId, txtName, txtDepartment, txtAddress};
-				boolean flag = false;
-				
-				for(int i = 0; i < check.length; i++) {
-					if(check[i].getText().length() == 0) {
-						flag = true;
-					}
-				}
-				
-				if(flag == true) {
-					JOptionPane.showMessageDialog(rootPane, "내용을 입력해주세요", "Error", JOptionPane.ERROR_MESSAGE);
-					return;
-				}
-				
-				JOptionPane.showMessageDialog(rootPane, "등록되었습니다", "Message", JOptionPane.INFORMATION_MESSAGE);
-				
-				for(int i = 0; i < check.length; i++) {
-					check[i].setText("");
-				}
-				
-				
-			}
-		});
-		
-		this.btnSelect = new JButton("목록");
-		this.add(btnSelect);
-		this.btnSelect.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try { 
-					Class.forName("com.mysql.jdbc.Driver");
-					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampledb", "root","58545256");
-					System.out.println("연결 성공");
-					
-					Statement stmt = conn.createStatement();
-					
-					ResultSet rs = stmt.executeQuery("select * from student order by name");
-					
-				    //JTable 초기화
-				    model.setNumRows(0);
-					
-					while(rs.next()) {
-//						System.out.println(rs.getString("id")); 
-//						System.out.println(rs.getString("name")); 
-//						System.out.println(rs.getString("dept"));						
-//						taList.append(rs.getString("id") + '\t' + rs.getString("name") + '\t' + rs.getString("dept") + '\n');
-						
-						String [] row = new String[4];
-						row[0]=rs.getString("id");
-						row[1]=rs.getString("name");
-						row[2]=rs.getString("dept");
-						row[3]=rs.getString("address");
-						model.addRow(row);
-						
-					}
-					rs.close();
-					stmt.close();
-					conn.close();
-				} catch (ClassNotFoundException e1) { e1.printStackTrace();
-				} catch (SQLException e1) {
-					System.out.println("연결 실패");
-					e1.printStackTrace();
-				}
-				
-			}
-			
-		});
-
-		this.btnUpdate = new JButton("수정");
-		this.add(btnUpdate);
-		this.btnUpdate.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try { 
-					Class.forName("com.mysql.jdbc.Driver");
-					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampledb", "root","58545256");
-					System.out.println("연결 성공");
-					
-					Statement stmt = conn.createStatement();
-					
-					//update
-					stmt.executeUpdate("update student set name='"+ txtName.getText() +"', dept='"+ txtDepartment.getText() + "', address='"+ txtAddress.getText() +"' where id = '"+ txtId.getText() +"'");
-					
-					//delete
-					//stmt.executeUpdate("delete from student where id = '1234567'");
-					
-					
-					ResultSet rs = stmt.executeQuery("select * from student where id = '"+ txtId.getText() +"' order by name");
-					
-				    //JTable 초기화
-				    model.setNumRows(0);
-					
-					while(rs.next()) {
-						
-//						taList.append(rs.getString("id") + '\t' + rs.getString("name") + '\t' + rs.getString("dept") + '\n');
-					
-						String [] row = new String[4];
-						row[0]=rs.getString("id");
-						row[1]=rs.getString("name");
-						row[2]=rs.getString("dept");
-						row[3]=rs.getString("address");
-						model.addRow(row);
-						
-					}
-					rs.close();
-					stmt.close();
-					conn.close();
-				} catch (ClassNotFoundException e1) { e1.printStackTrace();
-				} catch (SQLException e1) {
-					System.out.println("연결 실패");
-					e1.printStackTrace();
-				}
-			}
-			
-		});
-		
-		this.btnDelete = new JButton("삭제");
-		this.add(btnDelete);
-		this.btnDelete.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				int result = JOptionPane.showConfirmDialog(null, "삭제하시겠습니까?", "confirm", JOptionPane.YES_NO_CANCEL_OPTION);
-				if(result == JOptionPane.YES_OPTION) {
-					//Oracle 연동 삭제(delete)처리
-					
-					try { 
-						Class.forName("com.mysql.jdbc.Driver");
-						Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampledb", "root","58545256");
-						System.out.println("연결 성공");
-						
-						Statement stmt = conn.createStatement();
-						
-						//delete
-						stmt.executeUpdate("delete from student where id = '"+ txtId.getText() +"'");
-						
-						
-						ResultSet rs = stmt.executeQuery("select * from student order by name");
-						
-					    //JTable 초기화
-					    model.setNumRows(0);
-						
-						while(rs.next()) {
-							
-//							taList.append(rs.getString("id") + '\t' + rs.getString("name") + '\t' + rs.getString("dept") + '\n');
-						
-							String [] row = new String[4];
-							row[0]=rs.getString("id");
-							row[1]=rs.getString("name");
-							row[2]=rs.getString("dept");
-							row[3]=rs.getString("address");
-							model.addRow(row);
-						
-						
-						}
-						txtId.setText("");
-						txtName.setText("");
-						txtDepartment.setText("");
-						txtAddress.setText("");
-						rs.close();
-						stmt.close();
-						conn.close();
-					} catch (ClassNotFoundException e1) { e1.printStackTrace();
-					} catch (SQLException e1) {
-						System.out.println("연결 실패");
-						e1.printStackTrace();
-					}
-					
-					// 삭제 처리 후 메세지출력
-					JOptionPane.showMessageDialog(null, "삭제되었습니다", "message", JOptionPane.INFORMATION_MESSAGE);
-				}
-			}
-			
-		});
-		
-		this.setSize(320,500);
+		this.setSize(480,500);
 		this.setVisible(true);
 	}
 	
@@ -447,6 +93,7 @@ public class Haksa2 extends JFrame{
 		JMenuItem openLogin = new JMenuItem("Login");
 		JMenuItem miStudent = new JMenuItem("학생정보");
 		JMenuItem miBookRent = new JMenuItem("도서대출");
+
 		
 		openLogin.addActionListener(new ActionListener() {
 			@Override
@@ -464,6 +111,33 @@ public class Haksa2 extends JFrame{
 		mb.add(menu3);
 		
 		setJMenuBar(mb);
+		
+		miStudent.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			    panel.removeAll(); //모든컴포넌트 삭제
+			    panel.revalidate(); //다시 활성화
+			    panel.repaint();    //다시 그리기
+			    panel.add(new student()); //화면 생성.
+			    panel.setLayout(null);//레이아웃적용안함
+			}
+			
+		});
+		
+		miBookRent.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			    panel.removeAll(); //모든컴포넌트 삭제
+			    panel.revalidate(); //다시 활성화
+			    panel.repaint();    //다시 그리기
+			    panel.add(new bookRent()); //화면 생성.
+			    panel.setLayout(null);//레이아웃적용안함
+			}
+			
+		});
+		
 	}
 	
 	public static void main(String[] args) {
---
+++ govermentEdu_Java/Haksa2/src/bookRent.java
@@ -0,0 +1,132 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class bookRent extends JPanel{
+
+	DefaultTableModel model=null; // 테이블의 데이터
+	JTable table=null;
+	Connection conn=null;
+	
+	Statement stmt;  
+	String query; //sql문
+	
+	public bookRent() {
+		query="select s.id, s.name, b.title, br.rdate "
+				+ "from student s"
+				+ "join bookrent br"
+				+ "on s.id = br.id"
+				+ "join books b"
+				+ "on br.bookno = b.no";
+		
+		try {
+			//DB연결
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampledb", "root", "58545256");
+			System.out.println("연결완료");
+			stmt=conn.createStatement();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	    
+	    setLayout(null);//레이아웃설정. 레이아웃 사용 안함.
+	    
+	    JLabel l_dept=new JLabel("학과");
+	    l_dept.setBounds(10, 10, 30, 20);
+	    add(l_dept);
+	    
+	    String[] dept={"전체","ComputerSystem","MultiMedia","ComputerScience"};
+	    JComboBox cb_dept=new JComboBox(dept);
+	    cb_dept.setBounds(45, 10, 100, 20);
+	    add(cb_dept);
+	    
+	    cb_dept.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				query="select s.id, s.name, b.title, br.rdate "
+						+ " from student s"
+						+ " join bookrent br"
+						+ " on s.id = br.id"
+						+ " join books b"
+						+ " on br.bookno = b.no";
+				
+				JComboBox cb=(JComboBox)e.getSource(); //이벤트가 발생한 콤보박스 구하기
+				//동적쿼리
+				if(cb.getSelectedIndex()==0) {
+					//전체
+					query+=" order by br.no";
+				}else if(cb.getSelectedIndex()==1) {
+					//컴퓨터시스템
+					query+=" where s.dept='ComputerSystem' order by br.no";
+				}else if(cb.getSelectedIndex()==2) {
+					//멀티미디어
+					query+=" where s.dept='MultiMedia' order by br.no";
+				}else if(cb.getSelectedIndex()==3) {
+					//컴퓨터공학
+					query+=" where s.dept='ComputerScience' order by br.no";
+				}		
+				
+				//목록출력
+				list();
+				
+			}
+	    });
+	    
+	    String colName[]={"학번","이름","도서명","대출일"}; // 컬럼명
+	    model=new DefaultTableModel(colName,0); // 모델, 테이블의 데이터
+	    table = new JTable(model); // model과 table이 binding
+	    table.setPreferredScrollableViewportSize(new Dimension(470,200)); // 테이블크기
+	    add(table);
+	    JScrollPane sp=new JScrollPane(table); // 스크롤 구현
+	    sp.setBounds(10, 40, 460, 250);
+	    add(sp);  
+	    
+	    setSize(490, 400);//화면크기
+	    setVisible(true);
+	}
+	
+	public void list(){
+	    try{
+		     System.out.println(query);  //디버깅. 쿼리문을 출력.     
+		     // Select문 실행     
+		     ResultSet rs=stmt.executeQuery(query);
+		    
+		     //JTable 초기화
+		     model.setNumRows(0);
+		    
+		     while(rs.next()){
+		      String[] row=new String[4];//컬럼의 갯수가 4
+		      row[0]=rs.getString("id");
+		      row[1]=rs.getString("name");
+		      row[2]=rs.getString("title");
+		      row[3]=rs.getString("rdate");
+		      model.addRow(row); // model에 행 추가
+		     }
+		     rs.close();
+		    
+		    }
+		    catch(Exception e1){
+		     //e.getStackTrace();
+		     System.out.println(e1.getMessage());
+	    }							
+	 }
+	
+	public static void main(String[] args) {
+		new bookRent();
+	}
+
+}
