43ee6d27c86ad1bdbd062d2d388c15e9b1995376
padd60 <padd60@naver.com>
2021-11-22 16:45:25 +0900

    Spring : Reply CRUD API Use REST

---
+++ spring_workspace/EX02/src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -0,0 +1,24 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
+
+public interface ReplyMapper {
+	// 댓글등록
+	public int insert(ReplyVO vo);
+	
+	// 특정 댓글 읽기
+	public ReplyVO read(Long rno);
+	
+	// 댓글 삭제
+	public int delete(Long rno);
+	
+	// 댓글 수정
+	public int update(ReplyVO vo);
+	
+	// 댓글 페이징처리
+	public List<ReplyVO> getListWithPaging(@Param("cri") Criteria cri, @Param("bno") Long bno);
+}
