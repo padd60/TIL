45c8d1b9081594ccd725faee2db1efc7564884d3
padd60 <padd60@naver.com>
2021-12-07 18:57:53 +0900

    R :  start data analyst

--- r_workspace/script1.R
+++ r_workspace/script1.R
@@ -128,3 +128,35 @@ mpg
 mpg<-rename(mpg, city=cty)
 mpg<-rename(mpg, highway=hwy)
 head(mpg)
+
+install.packages("foreign")
+library(foreign)
+library(dplyr)
+library(ggplot2)
+library(readxl)
+raw_welfare <- read.spss(file = "Koweps_hpc10_2015_beta1.sav", to.data.frame = T)
+
+welfare <- raw_welfare
+welfare <- rename(welfare,
+                  sex = h10_g3,
+                  birth = h10_g4,
+                  marriage = h10_g10,
+                  religion = h10_g11,
+                  income = p1002_8aq1,
+                  code_job = h10_eco9,
+                  code_region = h10_reg7)
+table(welfare$sex)
+welfare$sex <- ifelse(welfare$sex == 9, NA, welfare$sex)
+table(is.na(welfare$sex))
+welfare$sex <- ifelse(welfare$sex == 1, "male", "female") 
+table(welfare$sex)
+qplot(welfare$sex)
+summary(welfare$income)
+qplot(welfare$income)
+qplot(welfare$income) + xlim(0, 1000)
+welfare$income <- ifelse(welfare$income %in% c(0, 9999), NA, welfare$income)
+table(is.na(welfare$income))
+sex_income <- welfare %>% filter(!is.na(income)) %>% group_by(sex) %>%
+  summarise(mean_income = mean(income))
+sex_income
+ggplot(data = sex_income, aes(x = sex, y = mean_income)) + geom_col()
